0265ccc6f5e033d6452a880eb64fb3a916a2ac27
trojanspike <trojan_spikepk@yahoo.co.uk>
2014-04-28 17:38:02 +0100

    vagrant boxes added , installer init added

--- installer.sh
+++ installer.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env bash
+
+
+function init {
+
+}
+
+
+## test which user
+if [ $UID -ne 0 ]; then
+	echo "You must run this script as root user"
+	exit 1
+else
+    # is root | sudo , run init
+	echo "@ root"
+	init
+fi
